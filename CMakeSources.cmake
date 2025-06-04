4b30906e8455f82b4aa02affd2a7b248893ee68c
Bob <robjohnson60@gmail.com>
2025-06-04 04:35:29 +0100

    - Added CMake wrapper to compile stb_image into a library.

---
+++ CMakeSources.cmake
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Copyright (c) Bob Johnson 2025 - All Rights Reserved
+#
+# Unauthorized copying of this file, via any medium is strictly prohibited.
+# Proprietary and confidential.
+#-------------------------------------------------------------------------------
+
+set(SRC_IMAGE
+	"stb_image.h"
+	"stb_image.c"
+)
---
+++ stb_image.c
@@ -0,0 +1,6 @@
+/* Copyright (c) Bob Johnson 2025 - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential. */
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
